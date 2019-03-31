feade5433d4f56074c19d18fc158d2983303b133
dilshod2017 <dilik2005@yahoo.com>
2019-03-31 15:18:59 -0400

    try hbs

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require("dotenv");
 const http    = require("http"),
 https   = require("https"),
 express = require("express"),
-fs      = require("fs");  
+fs      = require("fs"),
+hbs     = require("express-hbs");
 
       
     var app = express();
@@ -13,10 +13,19 @@ fs      = require("fs");
             res.redirect('https://' + req.headers.host + req.url);
         }
     });
+// app.set('port', process.env.PORT || 3000);
+app.set('trust proxy', 1) // trust first proxy
+app.use(express.static("public"));
+
+app.engine('hbs', hbs.express4({
+    partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views','./views');
 
 const mysql = require('mysql2');
 
-// create the connection to database
+// // create the connection to database
 var connection = mysql.createConnection({
     host: "mysql-project.ckfo5aylgcj5.us-east-2.rds.amazonaws.com",
     user: "root",
@@ -24,18 +33,25 @@ var connection = mysql.createConnection({
     port: 3306
 });
 
-connection.query(
+
+
+app.get("/", (req, res)=>{
+    connection.query(
     'SELECT * from student.users',
     function (err, results) {
         console.log(results);
     }
 );
 
+    res.render("home")
+})
+// connection.end();
 const option = {
     key: fs.readFileSync("key.pem"),
     cert: fs.readFileSync("server.crt"),
     passphrase: "localhost"
 }
 
+// http.createServer(app).listen(3000);
 https.createServer(option, app);
 http.createServer(app).listen(3000);
